e662c5a219a4bba4ce17928476fb5c7e3c4cb10c
Ronald Sit <mrronsit@gmail.com>
2021-12-06 20:44:13 -0600

    mutations/queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,63 @@
+const { gql } = require("apollo-server-express");
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(($bookData: BookInput!): User) {
+    saveBook(authors: $authors,
+        description: $description,
+        title: $title,
+        bookId: $bookId,
+        image: $image,
+        link: $link) {
+            bookId
+            authors
+            description
+            image
+            link
+            title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
